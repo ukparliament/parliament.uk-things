719cf17cf207b2481e2cfdd512c7e0934657b1f7
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-08-18 13:56:41 +0100

    [WEBSITE-1746] Create places/:place_id show page
    * Add places_controller
    * Add views
    * Add tests

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,23 @@
+class PlacesController < ApplicationController
+  before_action :data_check, :build_request
+
+  ROUTE_MAP = {
+    show:              proc { |params| Parliament::Utils::Helpers::ParliamentHelper.parliament_request.region_by_id.set_url_params({ region_code: params[:place_id] }) },
+  }.freeze
+
+  # Renders a single place given a place id.
+  # @controller_action_param :region_id [String] 8 character identifier that identifies constituency in graph database.
+  # @return [Grom::Node] object with type 'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'.
+
+  def show
+    @place, @constituencies = Parliament::Utils::Helpers::RequestHelper.filter_response_data(
+    @request,
+      'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion',
+      'http://id.ukpds.org/schema/ConstituencyGroup',
+      ::Grom::Node::BLANK
+    )
+
+    @place = @place.first
+
+  end
+end
