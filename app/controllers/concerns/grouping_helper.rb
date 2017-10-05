2905e8592bf7e09be6ba38ac11ba4352ed37b79f
Bryony Watson <watsonb@parliament.uk>
2017-10-05 10:58:47 +0100

    [WEBSITE-2059] Change - to 'to' and refactor
    * Changed - on previous consitutencies to 'to'
    * Updated helper to use 'grouping_methods' instead of 'optional_methods' for clarity

--- app/controllers/concerns/grouping_helper.rb
+++ app/controllers/concerns/grouping_helper.rb
@@ -12,24 +12,24 @@ module GroupingHelper
   # Groups objects by category where possible
   #
   # @param data [Array] Array of Grom::Nodes to be grouped
-  # @param optional_methods [Symbol] Optional number of methods can be called on Grom::Node to find suitable nodes for grouping
+  # @param grouping_methods [Symbol] One or more methods that should be called on Grom::Node to find suitable nodes for grouping
   # @return [Array] Single array of grouped, ungrouped and unknown objects
-  def self.group(data, *optional_methods)
-    grouped_data = group_data(data, optional_methods)
+  def self.group(data, *grouping_methods)
+    grouped_data = group_data(data, grouping_methods)
     create_sorted_array(grouped_data)
   end
 
-  # Where optional methods can be called (e.g. #category and #id), this method groups by the result of calling each of those methods in succession on each Grom::Node
+  # Where grouping methods can be called (e.g. #category and #id), this method groups by the result of calling each of those methods in succession on each Grom::Node
   # If a grouping cannot be found, adds 'UNKNOWN' as key of the hash
   #
   # @param data [Array] Array of Grom::Nodes to be grouped
-  # @param optional_methods [Array] Optional number of methods can be called on Grom::Node to find suitable nodes for grouping
+  # @param grouping_methods [Array] One or more methods that should be called on Grom::Node to find suitable nodes for grouping
   # @return [Hash] Keys identify grouping (grouped by category.property), with each value being an array of grouped, ungrouped and unknown Grom::Nodes
   #
   # e.g. { 12345678 => [...], 23456789 => [...], "UNKNOWN" => [...] }
-  def self.group_data(data, optional_methods)
+  def self.group_data(data, grouping_methods)
     data.group_by do |data_point|
-      optional_methods.inject(data_point) { |point, method| point.try(method) } || 'UNKNOWN'
+      grouping_methods.inject(data_point) { |point, method| point.try(method) } || 'UNKNOWN'
     end
   end
 
