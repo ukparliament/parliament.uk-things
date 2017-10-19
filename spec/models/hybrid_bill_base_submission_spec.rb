354618b3de0f12a24dd9cdb512bef0a91df536bc
ANGELA EBIRIM <ebirimac@gmail.com>
2017-10-19 11:41:00 +0100

    addition of petition id for petitoner model

--- spec/models/hybrid_bill_base_submission_spec.rb
+++ spec/models/hybrid_bill_base_submission_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HybridBillBaseSubmission, :type => :model do 
-
-  it "is valid" do 
-    hybrid_bill_base_model = build(:hybrid_bill_base_submission)
-  	expect(hybrid_bill_base_model).to be_valid
-  end	
-
+	context "model" do 
+		  it "is valid" do 
+		    hybrid_bill_base_model = build(:hybrid_bill_base_submission)
+		  	expect(hybrid_bill_base_model).to be_valid
+		  end	
+    end
 end	
