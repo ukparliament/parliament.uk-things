60c1d01fb04152f5253528ef929ff9da0c9e6044
ANGELA EBIRIM <ebirimac@gmail.com>
2017-10-23 12:04:02 +0100

    WIP custom validation

--- spec/factories/hybrid_bill_individual_submission.rb
+++ spec/factories/hybrid_bill_individual_submission.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do 
   factory :hybrid_bill_individual_submission do 
+    submitter_type 1
     first_name "susan"
     last_name "conkers"
     address_1 "millbank 7"
     address_2 "westminister"
     postcode "WC2 7TL"
     country "UK"
-    email "sconkers@gmail.com"
     telephone "0207 520 0890"
-    has_agent true
-    should_be_contacted false
+    email "testing@gmail.com"
+    receive_updates true
     committee_business_id 255
   end
 end
