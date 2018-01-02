e308ae5618043c0ea48b2280a9ef7526a0a1cd23
ANGELA EBIRIM <ebirimac@gmail.com>
2018-01-02 16:24:26 +0000

    add rspec tests for hybrid bill controller

--- spec/controllers/hybrid_bills_controller_spec.rb
+++ spec/controllers/hybrid_bills_controller_spec.rb
@@ -9,23 +9,47 @@ RSpec.describe HybridBillsController, vcr: false do
 
   describe 'GET show' do
     it_behaves_like 'successful show response'
-  end  
 
-  before(:each) do
-    Timecop.freeze(Time.utc(2017, 12, 1, 12, 0, 1))
-  end
+    describe 'submitted bill id' do 
+      it_behaves_like 'a correct bill id'
+    end
 
-  describe 'pre status' do
-    let(:stage) {'pre'}
-    let(:page)  {'hybrid_bills/pre'}
+    describe 'submitted incorrect bill id' do 
+      it_behaves_like 'an incorrect bill id'
+    end
 
-    it_behaves_like 'the current stage template'
-  end
+    context 'pre status' do
+      before(:each) do
+        Timecop.freeze(Time.utc(2017, 12, 1, 12, 0, 1))
+      end
 
- describe 'post status' do
-   let(:stage) {'post'}
-   let(:page)  {'hybrid_bills/post'}
+      it_behaves_like 'the expected hybrid bill stage', '1234', 'pre', 'hybrid_bills/pre'
+    end
 
-   it_behaves_like 'the not current stage template'
+    context 'active status' do
+      before(:each) do
+        Timecop.freeze(Time.utc(2018, 1, 6, 12, 0, 1))
+      end
+
+      it_behaves_like 'the expected hybrid bill stage', '1234', 'active', 'hybrid_bills/show'
+    end
+
+    context 'post status' do
+      before(:each) do
+        Timecop.freeze(Time.utc(2020, 1, 1, 12, 0, 1))
+      end
+
+      it_behaves_like 'the expected hybrid bill stage', '1234', 'post', 'hybrid_bills/post'
+    end
+
+    context 'closed status' do
+      before(:each) do
+        Timecop.freeze(Time.utc(2020, 1, 2, 12, 0, 1))
+      end
+
+      it 'redirects as expected' do
+        expect(get :show, params: { bill_id: '1234' }).to redirect_to(hybrid_bills_path)
+      end
+    end
   end
 end
--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -10,22 +10,30 @@ RSpec.shared_context 'successful show response' do
   end
 end
 
-RSpec.shared_context 'the current stage template' do
+RSpec.shared_context 'the expected hybrid bill stage' do |id, stage, page|
   before do
-    get :show, params: { bill_id: '1234', status: stage }
+    get :show, params: { bill_id: id, status: stage }
   end
 
-  it 'renders correctly' do
+  it "renders '#{page}' correctly in '#{stage}' stage" do
     expect(response).to render_template(page)
   end
 end
 
-RSpec.shared_context 'the not current stage template' do
-  before do
-    get :show, params: { bill_id: '1234', status: stage }
-  end
+RSpec.shared_context 'a correct bill id' do 
+ before do
+      get :show, params: { bill_id: '1234' }
+ end  
 
-  it 'does not render correctly' do
-    expect(response).not_to render_template(page)
-  end
+ it 'is ok' do 
+    expect(response.request.path_parameters[:bill_id]).to eq('1234')
+ end 
+
+end  
+
+RSpec.shared_context 'an incorrect bill id' do 
+ it 'is not ok' do 
+   expect {get :show, params: { bill_id: '90' }}.to raise_error(ActionController::RoutingError, 'invalid petition id')
+ end 
+
 end
