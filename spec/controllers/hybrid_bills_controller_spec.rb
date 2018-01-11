377ee80f734bc0802a747635b8d85e8da28eee25
Callum Neve-Jones <c.nevejones@gmail.com>
2018-01-11 11:58:56 +0000

    Fixed date reliant specs by fixing date using Timecop

--- spec/controllers/hybrid_bills_controller_spec.rb
+++ spec/controllers/hybrid_bills_controller_spec.rb
@@ -3,21 +3,29 @@ require 'rails_helper'
 RSpec.describe HybridBillsController, vcr: false do
   WebMock.disable_net_connect!
 
-     describe 'GET show' do 
-        it_behaves_like 'successful show response'
-     end  
+  after(:each) do
+   Timecop.return
+  end
 
-     describe 'pre status' do 
-        let(:stage) {'pre'}
-        let(:page) {'hybrid_bills/pre'}
+  describe 'GET show' do
+    it_behaves_like 'successful show response'
+  end  
 
-        it_behaves_like 'the current stage template'
-     end  
+  before(:each) do
+    Timecop.freeze(Time.utc(2017, 12, 1, 12, 0, 1))
+  end
 
-     describe 'post status' do 
-        let(:stage) {'post'}
-        let(:page) {'hybrid_bills/post'}
+  describe 'pre status' do
+    let(:stage) {'pre'}
+    let(:page)  {'hybrid_bills/pre'}
 
-        it_behaves_like 'the not current stage template'
-     end  
-end   
+    it_behaves_like 'the current stage template'
+  end
+
+ describe 'post status' do
+   let(:stage) {'post'}
+   let(:page)  {'hybrid_bills/post'}
+
+   it_behaves_like 'the not current stage template'
+  end
+end
