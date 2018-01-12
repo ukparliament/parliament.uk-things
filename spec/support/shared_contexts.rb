a72b4fce48e384398845d5dbcb252b0828b85a92
ANGELA EBIRIM <ebirimac@gmail.com>
2018-01-12 16:44:47 +0000

    renamed views wip hybrid bills controller specs

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -1,21 +1,14 @@
 require 'rails_helper'
 
-RSpec.shared_context 'successful show response' do
+RSpec.shared_context 'the expected hybrid bill stage' do |id, status, page|
   before do
-    get :show, params: { bill_id: '1234' }
+    params = { bill_id: id }
+    params[:status] = status if status
+
+    get :show, params: params
   end
 
-  it 'with 200 status code' do
-    expect(response).to have_http_status(200)
-  end
-end
-
-RSpec.shared_context 'the expected hybrid bill stage' do |id, stage, page|
-  before do
-    get :show, params: { bill_id: id, status: stage }
-  end
-
-  it "renders '#{page}' correctly in '#{stage}' stage" do
+  it "renders '#{page}' correctly in #{status ? "'#{status}'" : 'the current'} stage" do
     expect(response).to render_template(page)
   end
 end
@@ -35,13 +28,22 @@ RSpec.shared_context 'an incorrect bill id' do
  it 'is not ok' do 
    expect {get :show, params: { bill_id: '90' }}.to raise_error(ActionController::RoutingError, 'invalid petition id')
  end 
-end 
+end
 
-RSpec.shared_context 'a Hybrid Bills Individual Object' do
+RSpec.shared_context 'a hybrid bill simple step' do |id, step|
+  it 'renders the expected template' do
+    get :show, params: { bill_id: id, step: step }
 
-     before do 
+    expect(response).to have_rendered("hybrid_bills/steps/#{step}")
+  end
+end
 
-     
-     end
+RSpec.shared_context 'a hybrid bill cover page form' do |id, step, type|
+  it 'renders the expected template' do
+    post :show, params: { bill_id: id, step: step, type: type }
 
-end  
+    expect(response).to render_template("hybrid_bills/steps/forms/#{type}")
+  end
+end
+
+          
