3f2bdd9db9ba3d2ba8ffb3f2c945dadba9581db2
ANGELA EBIRIM <ebirimac@gmail.com>
2018-01-29 11:22:49 +0000

    addition of dates to hybrid bill stages for go live

--- app/models/hybrid_bill.rb
+++ app/models/hybrid_bill.rb
@@ -30,7 +30,7 @@ class HybridBill
 
   # Has to go after initialize because it initializes a HybridBill
   HYBRID_BILLS = {
-    '1' => ::HybridBill.new(Time.utc(2017, 12, 1, 12, 0, 0), Time.utc(2018, 1, 16, 9, 0, 0), Time.utc(2020, 1, 1, 9, 0, 0), Time.utc(2020, 1, 2, 9, 0, 0))
+    '1' => ::HybridBill.new(Time.utc(2017, 12, 1, 12, 0, 0), Time.utc(2018, 1, 30, 20, 0, 0), Time.utc(2018, 2, 26, 17, 0, 0), Time.utc(2020, 1, 2, 9, 0, 0))
   }.freeze
 
   def status
