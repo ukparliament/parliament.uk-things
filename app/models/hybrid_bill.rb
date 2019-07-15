7002f1997d3afbca229c8a858d17a2457814010e
Joshua Waheed <joshua.waheed@gmail.com>
2019-07-15 10:27:12 +0100

    [CTEWEB-494] Add start time for start date and end time for end date

--- app/models/hybrid_bill.rb
+++ app/models/hybrid_bill.rb
@@ -32,7 +32,7 @@ class HybridBill
     '1' => ::HybridBill.new(Time.utc(2017, 12, 1, 12, 0, 0), Time.utc(2018, 1, 30, 20, 0, 0), Time.utc(2018, 2, 26, 17, 0, 0), Time.utc(2020, 1, 2, 9, 0, 0)),
     '2' => ::HybridBill.new(Time.utc(2018, 3, 26, 12, 0, 0), Time.utc(2018, 3, 29, 00, 0, 1), Time.utc(2018, 4, 27, 13, 0, 0), Time.utc(2020, 1, 2, 9, 0, 0)),
     '3' => ::HybridBill.new(Time.utc(2019, 2, 13, 12, 0, 0), Time.utc(2019, 2, 15, 11, 0, 0), Time.utc(2019, 3, 15, 13, 0, 0), Time.utc(2020, 1, 2, 9, 0, 0)),
-    '4' => ::HybridBill.new(Time.utc(2019, 7, 1, 0, 0, 0), Time.utc(2019, 7, 17, 11, 0, 0), Time.utc(2019, 8, 16, 1, 0, 0), Time.utc(2020, 1, 1, 0, 0, 0))
+    '4' => ::HybridBill.new(Time.utc(2019, 7, 1, 0, 0, 0), Time.utc(2019, 7, 17, 10, 0, 0), Time.utc(2019, 8, 16, 1, 0, 0), Time.utc(2020, 1, 1, 0, 0, 0))
   }.freeze
 
   def status
